1ffeadcd5b75149227900a55ee5c8b95c188afc3
MuratYucedag <MurattYucedag@users.noreply.github.com>
2022-10-05 17:46:20 +0300

    Add project files.

---
+++ Logistic.UILayer/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Logistic.UILayer.Models;
+
+namespace Logistic.UILayer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        DBLogisticEntities db = new DBLogisticEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblCategory.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(TblCategory p)
+        {
+            db.TblCategory.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Logistic.UILayer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Logistic.UILayer.MvcApplication" Language="C#" %>

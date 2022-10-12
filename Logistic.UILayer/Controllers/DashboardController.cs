4e9df1f31cae082d8a1b69f0aa10ff469a4aa941
MuratYucedag <MurattYucedag@users.noreply.github.com>
2022-10-12 21:04:23 +0300

    Ders 14 sonu

---
+++ Logistic.UILayer/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Logistic.UILayer.Models;
+
+namespace Logistic.UILayer.Controllers
+{
+    public class DashboardController : Controller
+    {
+        DBLogisticEntities db = new DBLogisticEntities();
+        public ActionResult Index()
+        {
+            ViewBag.v1 = db.TblCategory.Count();
+            ViewBag.v2 = db.TblCustomer.Count();
+            ViewBag.v3 = db.TblCategory.Where(x => x.CategoryID == 1).Select(y => y.CategoryName).FirstOrDefault();
+            ViewBag.v4 = db.TblCategory.Where(x => x.CategoryStatus == true).Count();
+            return View();
+        }
+    }
+}
